648a59af8b7d3fef1522328f763f91452f5b9c2b
DaneshShahpouri <danesh.shahpouri@gmail.com>
2023-04-11 11:06:43 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import { store } from './store.js';
+import AppNavbar from './components/AppNavbar.vue';
+
+export default {
+  data() {
+    return {
+      store,
+
+    }
+  },
+
+  components: {
+    AppNavbar,
+  },
+}
+
+</script>
+
+<template>
+  <div class="main-container">
+    <AppNavbar></AppNavbar>
+  </div>
+</template>
+
+<style lang= "scss" scoped>
+@use './scss/variables' as *;
+</style>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,25 @@
+<script>
+import { store } from '../store.js';
+
+
+export default {
+  name: 'AppNavbar',
+
+  data() {
+    return {
+      store,
+
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="navbar-container">
+    ciao ciao
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../scss/variables' as *;
+</style>
